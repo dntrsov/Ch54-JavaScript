5a2e656c7158b826e2eea9a6bb8a615dffa81a44
Diana Ortiz <dntrsov@gmail.com>
2025-05-14 15:31:20 -0600

    feat: recursividad

--- js-02-functions/src/js/main.js
+++ js-02-functions/src/js/main.js
@@ -332,3 +332,79 @@ const imprimirEnDOMH2 = (mensaje) =>{
 }
 sumarEImprimir( 10, 14, imprimirEnDOMParagraph); // se imprime en paragraph
 sumarEImprimir( 10, 14, imprimirEnDOMH2); // se imprime en h2
+
+//sumarEImprimir(10, 14, alert);
+
+
+// Calcular el factorial de un número
+// factorial de 5: 5 * 4 * 3 * 2 * 1;
+function factorialConCicloFor( numero ) {
+    let factorial = 1;
+    for (let i=0; i < numero; i++) {
+      factorial *= (numero - i); // factorial = factorial * (numero - i);
+      console.log(`i: ${i}, factorial: ${factorial}, numero: ${numero - i}` );
+    }
+    return factorial;
+}
+console.log(`El factorial de 5 es: ${factorialConCicloFor(5)}`); // 120
+
+// calculando el factorial con recursividad
+
+function factorialConRecursividad( numero ){
+    console.log(`Resolviendo el factorial de ${numero}`);
+    if( numero <= 1  ) return 1 ;    
+    const result = factorialConRecursividad( numero - 1 ) * numero ;
+    console.log(`El factorial de ${numero} es ${result}`);
+    return result;
+}
+console.log(`Resultado final: ${ factorialConRecursividad(5)}`); // 120
+
+/*
+    parámetro   recursividad(n-1)   returnFnc
+    1            ----               1
+    2          2-1 = 1              2
+    3          3-1 = 2              6
+    4          4-1 = 3              24
+    5          5-1 = 4              120
+*/
+/*
+    parámetro   recursividad(n-1)   returnFnc
+     5              5-1 = 4            120
+     4              4-1 = 3            24
+     3              3-1 = 2            6       
+     2              2-1 = 1            2         
+     1               ----              1
+  
+*/
+
+/*
+ Generar una función recursiva que muestre en consola un saludo
+ donde se indique el número saludo deseado.
+  ej: saludar 10 veces
+  Saludo 1
+  Saludo 2
+  Saludo 3
+  Saludo 4
+  Saludo 5
+  Saludo 6
+   ....
+  Saludo 10
+*/
+
+function saludoConRecursividad( numeroHola ){
+    console.log(`Resolviendo el factorial de ${numeroHola}`);
+    if( numeroHola <= 10  ) return numeroHola ;    
+    const result = saludoConRecursividad( numeroHola - 1 ) * numeroHola ;
+    console.log(`El numero  de saludo ${hola} es ${result}`);
+    return result;
+}
+console.log(`Número de saludo final: ${ saludoConRecursividad(10)}`); 
+
+//David 
+const saludos = numeroDeSaludos => {
+  if (numeroDeSaludos === 0) return;
+  saludos(numeroDeSaludos - 1);
+  console.log(`Saludo ${numeroDeSaludos}`);
+}
+saludos(10);
+
