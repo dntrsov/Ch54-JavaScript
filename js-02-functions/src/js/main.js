f8f1204bb1f2c4eecd15a9c4da22dcc6c6e98e40
Diana Ortiz <dntrsov@gmail.com>
2025-05-14 13:33:42 -0600

    feat: ejericio callback multifuncional imprimir p,h2,console

--- js-02-functions/src/js/main.js
+++ js-02-functions/src/js/main.js
@@ -236,6 +236,14 @@ saludarPersona(); //Hola undifined, ya nos vamos..
 saludarPersona("Mich"); // Hola Mich, ...
 
 
+console.log(parseInt("5")); //5
+console.log(parseInt("1000")); // 1000 (base 10 0,1,2,3,4,5,6,7,8,9)
+console.log(parseInt("1000", 10)); //1000 (base 10)
+console.log(parseInt("1000", 2)); // 8 (base 10)
+console.log(parseInt("D2042D")); //NaN base 10
+console.log(parseInt("D2042D", 16)); //13763629
+
+
 
 /*
  ------------ Funciones de Callback -----------------------
@@ -245,6 +253,65 @@ saludarPersona("Mich"); // Hola Mich, ...
  */
 
 
+const imprimirMensaje= ( fnCallBack ) => fnCallBack("Hola Ch54"); 
+// esta fn va a trabjar sobre el entendi de que lo que le va allegar es una referencia de una f(), es un alias, la está invicando.
 
+//imprimirMensaje( 18 ); 
+// 18("Hola Ch54");
+
+// fnCallBack is not a function
+//imprimirMensaje("Patito"); // "patito" ("HolaCh54"); ERROR
+
+imprimirMensaje( console.log ); //sin parentesis, si le pone "" lo estaria ejecuatando, no queremos que se ejecute. Console.log("Hola..")
+//Le estamos pasando la pura referencia, no el valor, las fx son onjetos, REFERENCIA DIF VALOR. 
+//console.log(Hola ch54)
+
+//imprimirMensaje( console.log("Luis")); // Luis
+//imprimirMensaje(undefined); // Luis
+
+const enviarAParrafo = (mensaje) => {
+  const saluda = "Hola buen día";
+  const referencia = document.getElementById("saludo-callback");
+  referencia.innerHTML = `${saluda} ${mensaje}`;
+
+};
+imprimirMensaje(enviarAParrafo);
+
+//Usando la función imprimirMensaje, enviar un callback para que imprima con alert
+//imprimirMensaje( alert);
 
   
+/*
+  Realizar una función que sume dos numeros y que imprima
+  el resultado.
+   - inicialmente se imprimirá en la consola
+   - es posible que se te pida imprimir en el dom
+   - es posible que se te pida imprimir en un alert
+*/
+/*const sumarDosNumeros = ( a, b) => a  + b;
+const sumarEImprimir = ( a, b) => {
+   const resultado = sumarDosNumeros(a, b);
+   const mensaje = `La suma de ${a} + ${b} es: ${resultado}`;
+   // imprimir en consola
+   // console.log(mensaje);
+   document.getElementById("resultado-sumatoria").innerText = mensaje;
+}
+sumarEImprimir( 10, 14);*/
+
+/*const sumarEImprimir = ( a, b , opcion = "consola") => {
+   const resultado = sumarDosNumeros(a, b);
+   const mensaje = `La suma de ${a} + ${b} es: ${resultado}`;
+    //imprimir en consola
+   if( opcion === "consola") console.log(mensaje);
+   else if (opcion === "parrafo") document.getElementById("resultado-sumatoria").innerText = mensaje;
+   else if (opcion === "h2")document.getElementById("resultadoh2-sumatoria").innerText = mensaje;
+   else alert(mensaje)
+}
+sumarEImprimir( 10, 14);
+sumarEImprimir(50,50, "parrafo");
+*/
+/*const sumarEImprimir = (a, b, imprimir = console.log) =>{
+  const resultado = sumarDosNumeros(a, b);
+  const mensaje = `La suma de ${a} + ${b} es: ${resultado}`;
+
+}*/S
